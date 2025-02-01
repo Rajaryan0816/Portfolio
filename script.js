bc70951f04a8958c0d206421b33a0de80be913b5
Rajaryan0816 <aryan08162@gmail.com>
2025-02-02 01:14:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Form Submission Handling
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    // Simulate form submission
+    alert(`Thank you, ${name}! Your message has been sent.`);
+    document.getElementById('contact-form').reset();
+  });
